0ef745b33b7f7e62819b4f4fd80d49929bfbc9c0
Даниил <danya.45san@outlook.com>
2022-11-01 11:02:08 +0300

    Принт суммы двух чисел.

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
-        System.out.println(a);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
     }
 }
